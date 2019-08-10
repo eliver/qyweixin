e0c4e90e76306e22686113edf877716b075ba8b6
Eric Leong <eliver@yahoo.com>
2019-08-11 00:53:09 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# zabbix 配置
+ZABBIX_URL = 'http://20.189.74.239/zabbix/'
+ZABBIX_USERNAME = 'Admin'
+ZABBIX_PASSWD = 'zabbix'
+
+# 企业微信配置
+TOKEN = "k2Y47ZI5wwGYW"
+EncodingAESKey = "hofUWeNhjRjJdWKaJh15w4xvzBhJXOau69wqHiVwLiS"
+CorpId = "eliver"
+Secret = "ww6a0692b909e55136"
+
+# 图灵机器人key
+turing_key = 'a12392eb44294f24866533a28e261d04'
+
+TMP_DIR = "./tmp"
